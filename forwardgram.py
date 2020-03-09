d14e7714c86089b9504670472579e6f83e808695
Tommy <me@tomerlev.com>
2020-03-09 22:36:08 +0200

    Added support for multiple forwarding channels. Switched to use to yaml.safe_load. Added more documentation in the sample config file

--- forwardgram.py
+++ forwardgram.py
@@ -16,28 +16,34 @@ def start(config):
     client.start()
 
     input_channels_entities = []
-    output_channel_entity = None
+    output_channel_entities = []
     for d in client.iter_dialogs():
         if d.name in config["input_channel_names"]:
             input_channels_entities.append(InputChannel(d.entity.id, d.entity.access_hash))
-        if d.name == config["output_channel_name"]:
-            output_channel_entity = InputChannel(d.entity.id, d.entity.access_hash)
+        if d.name in config["output_channel_names"]:
+            output_channel_entities.append(InputChannel(d.entity.id, d.entity.access_hash))
             
-    if output_channel_entity is None:
-        logger.error(f"Could not find the channel \"{config['output_channel_name']}\" in the user's dialogs")
+    if not output_channel_entities:
+        logger.error(f"Could not find any output channels in the user's dialogs")
         sys.exit(1)
-    logging.info(f"Listening on {len(input_channels_entities)} channels. Forwarding messages to {config['output_channel_name']}.")
+
+    if not input_channels_entities:
+        logger.error(f"Could not find any input channels in the user's dialogs")
+        sys.exit(1)
+        
+    logging.info(f"Listening on {len(input_channels_entities)} channels. Forwarding messages to {len(output_channel_entities)} channels.")
     
     @client.on(events.NewMessage(chats=input_channels_entities))
     async def handler(event):
-        await client.forward_messages(output_channel_entity, event.message)
+        for output_channel in output_channel_entities:
+            await client.forward_messages(output_channel, event.message)
 
     client.run_until_disconnected()
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
-        print(f"Usage: {sys.argv[0]} CONFIG_PATH")
+        print(f"Usage: {sys.argv[0]} {{CONFIG_PATH}}")
         sys.exit(1)
     with open(sys.argv[1], 'rb') as f:
-        config = yaml.load(f)
+        config = yaml.safe_load(f)
     start(config)
