5c6627200c6645e926b81b0e2cee3610b2e0a543
Raf Theunis <raf.theunis@kbc.be>
2020-08-27 13:08:27 +0200

    feat(send): use send instead of forward

--- forwardgram.py
+++ forwardgram.py
@@ -36,7 +36,7 @@ def start(config):
     @client.on(events.NewMessage(chats=input_channels_entities))
     async def handler(event):
         for output_channel in output_channel_entities:
-            await client.forward_messages(output_channel, event.message)
+            await client.send_message(output_channel, event.message)
 
     client.run_until_disconnected()
 
